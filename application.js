9886b50511c5af3e5407afb13b98cd61e271405f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-03-31 11:19:27 -0400

    The Pen Centre 2.1  /application.js

--- application.js
+++ application.js
@@ -323,15 +323,16 @@ function renderEventDetails(container, template, collection, mall_name){
             val.promo_image_show="display:none";
         }
         
-        var show_date = new Date (val.show_on_web_date + "T04:00:00Z");
-        start = new Date (val.start_date + "T04:00:00Z");
-        end = new Date (val.end_date + "T04:00:00Z");
-    
-        if (start.toDateString() == end.toDateString()) {
-            val.dates = (get_month(start.getMonth()))+" "+(start.getDate());    
-        } else {
-            val.dates = (get_month(start.getMonth()))+" "+(start.getDate())+" - "+get_month(end.getMonth())+" "+end.getDate();    
+        ar show_date = moment(val.show_on_web_date).tz(getPropertyTimeZone());
+        var start = moment(val.start_date).tz(getPropertyTimeZone());
+        var end = moment(val.end_date).tz(getPropertyTimeZone());
+        if (start.format("DMY") == end.format("DMY")){
+        	val.dates = start.format("MMM D");
         }
+        else {
+        	val.dates = start.format("MMM D") + " - " + end.format("MMM D");
+        }
+        
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
     });
